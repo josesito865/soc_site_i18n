94997a96ea6133d73e371ff59552ad4919ac1f87
jorgev259 <jorgev259@outlook.es>
2022-04-15 17:05:09 -0600

    First Commit

---
+++ check.js
@@ -0,0 +1,27 @@
+const fs = require('fs-extra')
+const glob = require('glob')
+const path = require('path')
+
+const langs = require('./langs.json')
+
+const baseList = glob.sync('en/**.json')
+baseList.forEach(space => {
+  langs.forEach(lang => {
+    const base = require(`./${space}`)
+    const fileName = space.replace('en/', '')
+    const langFile = require(`./${lang}/${fileName}`)
+
+    const add = Object.keys(base).filter(k => !langFile[k])
+    const remove = Object.keys(langFile).filter(k => !base[k])
+
+    add.forEach(k => {
+      langFile[k] = base[k]
+    })
+
+    remove.forEach(k => {
+      delete langFile[k]
+    })
+
+    if (add.length > 0 || remove.length > 0) fs.writeJsonSync(path.join(lang, fileName), langFile, { spaces: 2 })
+  })
+})
